06a10fc8866a9410d54c3acc1fd566750c44d1b5
Lenon <lenonmauer@gmail.com>
2019-03-01 16:55:54 -0300

    changed background to white

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   border-radius: 3px;
+  background: #fff;
   width: 300px;
 `;
 
